43de01d028309ab835b9e35f26fbd6f0df96f941
Ranger Leon <ranger.leon@Y22-Design-Ranger-Leon.local>
2018-09-19 12:16:55 -0400

    slope cal moddified

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -18,7 +18,10 @@ y2 = int(y2)
 rise = x2 - x1
 run = y2 - y1
 
+fSlope = rise/run
 
 #Output
+print("Your slope is m = "+str(rise)+"/"+str(run))
+print("Your slope as a decimal is "+str(fSlope))
 print(rise)
 print(run)
