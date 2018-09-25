54c0e7fecda2bf5f4b8e888f96775c7f45dd81c7
Ranger Leon <ranger.leon@Y22-Design-Ranger-Leon.local>
2018-09-25 09:11:46 -0400

    Imput of volume of a cylinder

---
+++ CylinderVolGUI.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+root = tk.Tk() 
+root.title("Volume of a Cylinder")
+
+labr = tk.Label(root, text="radius")
+labr.pack()
+
+entr = tk.Entry(root)
+entr.pack()
+
+labh = tk.Label(root, text="height")
+labh.pack()
+
+enth = tk.Entry(root)
+enth.pack()
+
+btn = tk.Button(root, text ="Submit")
+btn.pack()
+
+output= tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
+output.config(state="disabled")
+output.pack()
+
+
+
+
+
+root.mainloop()
+print("end program")
