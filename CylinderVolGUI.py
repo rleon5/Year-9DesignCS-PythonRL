293c3ffba3857d3c6f04dec1bddf1c919841c7a8
Ranger Leon <ranger.leon@Y22-Design-Ranger-Leon.local>
2018-09-27 15:05:40 -0400

    modefied process of gui cylindar

--- CylinderVolGUI.py
+++ CylinderVolGUI.py
@@ -1,8 +1,24 @@
 import tkinter as tk
+import math
 
+
+# When you are dealing with event driven programming the program sits and waits for the user
+# to do something. These are programs where you click a button and something happens. What we
+# we do is BIND a function to a key.  So when we click the button the function runs. 
+#
 def submit():
 
+	print("Submit pressed")
 
+	r =float(entr.get())
+	h =float(enth.get())
+
+	v = math.pi*r*r*h
+	v = round(v,3)
+
+	output.config(state="normal")
+	output.insert(tk.INSERT,v)
+	output.config(state="disabled")
 
 root = tk.Tk() 
 root.title("Volume of a Cylinder")
@@ -19,7 +35,7 @@ labh.pack()
 enth = tk.Entry(root)
 enth.pack()
 
-btn = tk.Button(root, text ="Submit")
+btn = tk.Button(root, text ="Submit", command = submit)
 btn.pack()
 
 output= tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
