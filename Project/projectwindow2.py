3c8879d271e5416d5ed4b5f6eb67bf2b094b9f6c
Ranger Leon <ranger.leon@Y22-Design-Ranger-Leon.local>
2018-12-07 15:23:32 -0500

    updated projectwindow2

--- Project/projectwindow2.py
+++ Project/projectwindow2.py
@@ -4,11 +4,8 @@ from tkinter import *
 
 from tkinter import messagebox
 
-def submitData():
-
-	messagebox.showinfo("Form Data has Been Submited", "Thank you for submiting: " + nameEntry.get() + ageEntry.get() + "!")
-	userName = waterEntry.get() + sleepEntry.get()
-	print("Submit pressed")
+def submit(*args):
+	print("Water Entry: ", waterEntry.get(), "Sleep: ", sleepEntry.get())
 
 root = tk.Tk()
 
@@ -18,24 +15,20 @@ root.title("How Active Are You")
 
 sleepLabel = tk.Label(root, text = "How much sleep did you get today in hours?")
 sleepLabel.grid(row = 0, column = 1)
-waterEntry = tk.Entry(root)
-waterEntry.grid(row=1, column=1)
+sleepEntry = tk.Entry(root)
+sleepEntry.grid(row=1, column=1)
 
 waterLabel = tk.Label(root, text = "How much water did you get today in litres?")
 waterLabel.grid(row = 2, column = 1)
 
 waterEntry = tk.Entry(root)
 waterEntry.grid(row=3, column=1)
+	
 
-def onclick(args):
+btn1 = tk.Button(root, text = "Submit Data", command = submit)
+btn1.grid(row=5, column=1)
 
-	if args == 1:
-		print("Submitted")
-submitButton = tk.Button(root, text = "Submit", command =lambda:onclick(1))
-submitButton.grid(row=5, column=1)
+btn1.grid()
 
-def num():
-print ("num in sleepLabel")
-
-
+ 
 root.mainloop()
